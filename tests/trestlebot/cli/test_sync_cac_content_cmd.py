65890ad306c50b726e8d586829305ab81a2007e4
Hannah Braswell <hbraswel@redhat.com>
2025-03-10 12:03:26 -0400

    fix: tests updated to resolve cac profile
    This commit updates the unit tests to
    validate the entry of a single profile
    title instead of a path.

--- tests/trestlebot/cli/test_sync_cac_content_cmd.py
+++ tests/trestlebot/cli/test_sync_cac_content_cmd.py
@@ -26,8 +26,7 @@ test_product = "rhel8"
 # Note: data in test_content_dir is copied from content repo, PR:
 # https://github.com/ComplianceAsCode/content/pull/12819
 test_content_dir = TEST_DATA_DIR / "content_dir"
-test_cac_profile = "products/rhel8/profiles/example.profile"
-test_cac_profiler = f"{test_content_dir}/products/{test_product}/profiles/example.profile"
+test_cac_profile = "example"
 test_prof = "simplified_nist_profile"
 test_cat = "simplified_nist_catalog"
 test_comp_path = f"component-definitions/{test_product}/component-definition.json"
@@ -267,7 +266,7 @@ def test_sync_product(tmp_repo: Tuple[str, Repo]) -> None:
             "--cac-content-root",
             test_content_dir,
             "--cac-profile",
-            test_cac_profiler,
+            test_cac_profile,
             "--oscal-profile",
             test_prof,
             "--committer-email",
